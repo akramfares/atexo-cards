ff52eaa02b689b1fd1c47ebe48ef21b83e394d09
Akram Fares <akram.fares@capgemini.com>
2016-07-11 18:31:36 +0200

    - Installation du bundle Guzzle - Création du Test Fonctionnel - Initialisation du controller principal

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
@@ -13,9 +14,18 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+        try {
+            $client = $this->get('guzzle.client.api_atexo');
+            $json = $client->request('GET', '/test/cards/57187b7c975adeb8520a283c')->getBody();
+            $result = $this->sortCards($json);
+        } catch (\Exception $e) {
+            return new JsonResponse($e->getMessage());
+        }
+        return new JsonResponse($result);
+    }
+
+    private function sortCards($json)
+    {
+        return [];
     }
 }
--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -1,6 +1,10 @@
 <?php
 
 namespace AppBundle\Tests\Controller;
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
@@ -8,11 +12,37 @@ class DefaultControllerTest extends WebTestCase
 {
     public function testIndex()
     {
-        $client = static::createClient();
+    	// Exemple de cartes non ordonnées
+    	$json = '{"exerciceId":"5782cc03975adeb8520a3ade","dateCreation":1468189699577,"candidate":{"candidateId":"57187b7c975adeb8520a283c","firstName":"Othmane","lastName":"QABLAOUI"},"data":{"cards":[{"category":"CLUB","value":"KING"},{"category":"CLUB","value":"TWO"},{"category":"HEART","value":"FOUR"},{"category":"SPADE","value":"SEVEN"},{"category":"CLUB","value":"EIGHT"},{"category":"HEART","value":"TWO"},{"category":"CLUB","value":"THREE"},{"category":"DIAMOND","value":"NINE"},{"category":"HEART","value":"KING"},{"category":"SPADE","value":"TEN"}],"categoryOrder":["DIAMOND","HEART","SPADE","CLUB"],"valueOrder":["ACE","TWO","THREE","FOUR","FIVE","SIX","SEVEN","EIGHT","NINE","TEN","JACK","QUEEN","KING"]},"name":"cards"}';
+        
+        // Création du mock du client Guzzle
+        $mock = new MockHandler([
+		    new Response(200, ['Content-Type' => 'application/json'] ,$json),
+		]);
+		$handler = HandlerStack::create($mock);
+		$mockGuzzle = new Client(['handler' => $handler]);
 
+		// Création du client symfony pour faire le test fonctionnel
+		$client = static::createClient();
+		// Mock du service Guzzle
+		$client->getContainer()->set('guzzle.client.api_atexo', $mockGuzzle);
+
+		// Appel au controller
         $crawler = $client->request('GET', '/');
 
+        // Vérifier le status HTTP de la réponse
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        // Vérifier le type de contenu retourné
+        $this->assertTrue(
+	    $client->getResponse()->headers->contains(
+		        'Content-Type',
+		        'application/json'
+		    ),
+		    'the "Content-Type" header is "application/json"'
+		);
+        // Vérifier si les cartes sont bien ordonnées
+		$this->assertContains('{"cards":[{"category":"DIAMOND","value":"NINE"},{"category":"HEART","value":"TWO"},{"category":"HEART","value":"FOUR"},{"category":"HEART","value":"KING"},{"category":"SPADE","value":"SEVEN"},{"category":"SPADE","value":"TEN"},{"category":"CLUB","value":"TWO"},{"category":"CLUB","value":"THREE"},{"category":"CLUB","value":"EIGHT"},{"category":"CLUB","value":"KING"}],"categoryOrder":["DIAMOND","HEART","SPADE","CLUB"],"valueOrder":["ACE","TWO","THREE","FOUR","FIVE","SIX","SEVEN","EIGHT","NINE","TEN","JACK","QUEEN","KING"]}'
+			, $client->getResponse()->getContent()
+		);
     }
 }
